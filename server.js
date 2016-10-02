ae88dcde283fb9f65b3725b7d5524782592405a6
oilylyme <elynorfreyss@gmail.com>
2016-10-02 16:21:00 -0400

    Initial Deployment Adjustments

--- server.js
+++ server.js
@@ -4,3 +4,6 @@ var app = express();
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+
+exports.app = app;
---
+++ test/test.js
@@ -0,0 +1,21 @@
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+
+chai.use(chaiHttp);
+
+describe('Basic Server', function() {
+    it('should load a root page when hitting the root url',function(done) {
+        chai.request(app)
+            .get('/')
+            .end(function(err, res) {
+                should.equal(err, null);
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            });
+    });
+});
