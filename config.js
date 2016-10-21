b643cc064833cbc7e093e917ed5e3e88f3a17ab9
oilylyme <elynorfreyss@gmail.com>
2016-10-20 23:49:34 -0400

    updated config for heroku mLab

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://oilylyme:Weareart5@ds011860.mlab.com:11860/drops-in-the-bucket' :
+                            'mongodb://userone:genericpass@ds011860.mlab.com:11860/drops-in-the-bucket' :
                             'mongodb://localhost/room-control-dev');
 exports.PORT = process.env.PORT || 8080;
